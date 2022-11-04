923f7aa9791b01658cc0217fe20699ef857dcee0
hayow <Hayow96@gmail.com>
2022-11-03 22:50:34 -0500

    Approach: 2

--- Dynamic Programming/Array/Medium/Leetcode Problem #198.playground/Contents.swift
+++ Dynamic Programming/Array/Medium/Leetcode Problem #198.playground/Contents.swift
@@ -40,3 +40,38 @@ func approach1(_ array: [Int]) -> Int {
 
 
 
+/*
+
+ Approach: 2
+ Method: Recursion + Memoization
+ Time complexity: O(n), n == height of the recursive tree
+ Space complexity: O(n), n == height of the recursive tree
+ 
+*/
+
+
+
+func approach2(_ array: [Int]) -> Int {
+    var cache: [Int: Int] = [:]
+    func helper(_ _array: [Int]) -> Int {
+        if _array.isEmpty {
+            return 0
+        } else if let cachedValue: Int = cache[0] {
+            return cachedValue
+        } else if _array.count == 1 {
+            return _array[0]
+        } else if _array.count == 2 {
+            return max(_array[0], _array[1])
+        } else {
+            cache[0] = max(_array[0] + helper(Array(_array[2..<_array.count])), helper(Array(_array[1..<_array.count])))
+            return cache[0] ?? 0
+        }
+    }
+    
+    return helper(array)
+}
+
+
+
+
+
