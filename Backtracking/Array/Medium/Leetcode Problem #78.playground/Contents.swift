2089d24a35bf058319d4bde92b5a7b39f95819ee
hayow <Hayow96@gmail.com>
2022-11-03 21:56:34 -0500

    Approach: 1

--- Backtracking/Array/Medium/Leetcode Problem #78.playground/Contents.swift
+++ Backtracking/Array/Medium/Leetcode Problem #78.playground/Contents.swift
@@ -13,3 +13,35 @@
 
 
 
+/*
+
+ Approach: 1
+ Method: Recursion
+ Time complexity: O(2^n), n == array.count
+ Space complexity: O(2^n), n == height of the recursive tree == array.count
+ 
+*/
+
+
+
+func approach1(_ array: [Int]) -> [[Int]] {
+    
+    func helper(_ array1: [Int], _ array2: [Int]) -> [[Int]] {
+        if array1.isEmpty {
+            
+            return [array2]
+        } else {
+            var subsets: [[Int]] = helper(Array(array1[1..<array1.count]), array2)
+            subsets = subsets + helper(Array(array1[1..<array1.count]), [array1[0]] + array2)
+            
+            return subsets
+        }
+    }
+    
+    return helper(array, [])
+}
+
+
+
+
+
