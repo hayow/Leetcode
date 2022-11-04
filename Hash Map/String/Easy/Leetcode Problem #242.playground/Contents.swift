8df4ea4453556635ff9530db19c7169e156a4dd2
hayow <Hayow96@gmail.com>
2022-11-04 08:27:13 -0500

    Approach: 2

--- Hash Map/String/Easy/Leetcode Problem #242.playground/Contents.swift
+++ Hash Map/String/Easy/Leetcode Problem #242.playground/Contents.swift
@@ -17,7 +17,7 @@
 
  Approach: 1
  Method: NA
- Time complexity: O(nlogn), n == max(string1.count, string2.count)
+ Time complexity: O(nlogn + mlogm), m == string1.count, n == string2.count
  Space complexity: O(m + n), m == string1.count, n == string2.count
  
 */
@@ -29,6 +29,44 @@ func approach1(_ string1: String, _ string2: String) -> Bool {
     let sortedString2: String = String(string2.sorted())
     
     return sortedString1 == sortedString2
+}
+
+
+
+
+
+/*
+
+ Approach: 2
+ Method: Iteration
+ Time complexity: O(m + n), m == string1.count, n == string2.count
+ Space complexity: O(m + n), m == string1.count, n == string2.count
+ 
+*/
+
+
+
+func approach2(_ string1: String, _ string2: String) -> Bool {
+    var string1HashMap: [Character: Int] = [:]
+    var string2HashMap: [Character: Int] = [:]
+    
+    for character in string1 {
+        if let count: Int = string1HashMap[character] {
+            string1HashMap[character] = count + 1
+        } else {
+            string1HashMap[character] = 1
+        }
+    }
+    
+    for character in string2 {
+        if let count: Int = string2HashMap[character] {
+            string2HashMap[character] = count + 1
+        } else {
+            string2HashMap[character] = 1
+        }
+    }
+    
+    return string1HashMap == string2HashMap
 }
 
 
