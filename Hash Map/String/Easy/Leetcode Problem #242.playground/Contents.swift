e7a7c2e7019156ab4a30ad92bdfc0ba8c06cdd72
hayow <Hayow96@gmail.com>
2022-11-04 08:19:33 -0500

    Approach: 1

--- Hash Map/String/Easy/Leetcode Problem #242.playground/Contents.swift
+++ Hash Map/String/Easy/Leetcode Problem #242.playground/Contents.swift
@@ -13,3 +13,25 @@
 
 
 
+/*
+
+ Approach: 1
+ Method: NA
+ Time complexity: O(nlogn), n == max(string1.count, string2.count)
+ Space complexity: O(m + n), m == string1.count, n == string2.count
+ 
+*/
+
+
+
+func approach1(_ string1: String, _ string2: String) -> Bool {
+    let sortedString1: String = String(string1.sorted())
+    let sortedString2: String = String(string2.sorted())
+    
+    return sortedString1 == sortedString2
+}
+
+
+
+
+
