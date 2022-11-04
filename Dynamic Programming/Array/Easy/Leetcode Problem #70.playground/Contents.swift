4dc15934bbd7a863e4243f41c641abc426f886c9
hayow <Hayow96@gmail.com>
2022-11-04 09:08:58 -0500

    Approach: 1

--- Dynamic Programming/Array/Easy/Leetcode Problem #70.playground/Contents.swift
+++ Dynamic Programming/Array/Easy/Leetcode Problem #70.playground/Contents.swift
@@ -13,3 +13,28 @@
 
 
 
+/*
+
+ Approach: 1
+ Method: Recursion
+ Time complexity: O(2^n), n == height of the recursive tree
+ Space complexity: O(n), n == height of the recursive tree
+ 
+*/
+
+
+
+func approach1(_ steps: Int) -> Int {
+    if steps == -1 {
+        return 0
+    } else if steps == 0 {
+        return 1
+    } else {
+        return approach1(steps - 1) + approach1(steps - 2)
+    }
+}
+
+
+
+
+
