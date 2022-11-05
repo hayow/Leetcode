3a75a395a97e9ed2092527f6c2c1d3b5afa0da5c
hayow <Hayow96@gmail.com>
2022-11-05 11:00:11 -0500

    Approach: 1

--- Hash Map/Array/Easy/Leetcode Problem #1.playground/Contents.swift
+++ Hash Map/Array/Easy/Leetcode Problem #1.playground/Contents.swift
@@ -13,3 +13,29 @@
 
 
 
+/*
+
+ Approach: 1
+ Method: Iteration
+ Time complexity: O(n^2), n == array.count
+ Space complexity: O(1)
+ 
+*/
+
+
+
+func approach1(_ array: [Int], _ target: Int) -> [Int] {
+    
+    for i in 0..<array.count {
+        for j in 1 + i..<array.count {
+            if array[i] + array[j] == target { return [i, j] }
+        }
+    }
+    
+    return []
+}
+
+
+
+
+
