6347779d88dfc7b43bfd42e3f776bebbd67d293a
hayow <Hayow96@gmail.com>
2022-11-05 16:42:44 -0500

    Approach: 1

--- Stack/Hash Map/Easy/Leetcode Problem #20.playground/Contents.swift
+++ Stack/Hash Map/Easy/Leetcode Problem #20.playground/Contents.swift
@@ -13,3 +13,42 @@
 
 
 
+/*
+
+ Approach: 1
+ Method: Iteration
+ Time complexity: O(n), n == string.count
+ Space complexity: O(n), n == parenthesesStack.count
+ 
+*/
+
+
+
+func approach1(_ string: String) -> Bool {
+    let parenthesesMap: [Character: Character] = [
+        ")": "(",
+        "]": "[",
+        "}": "{"
+    ]
+    var parenthesesStack: [Character] = []
+    
+    for parenthese in string {
+        
+        if let _parenthese: Character = parenthesesMap[parenthese] {
+            if parenthesesStack.isEmpty == false && parenthesesStack[parenthesesStack.count - 1] == _parenthese {
+                parenthesesStack.removeLast()
+            } else {
+                return false
+            }
+        } else {
+            parenthesesStack.append(parenthese)
+        }
+    }
+    
+    return parenthesesStack.isEmpty
+}
+
+
+
+
+
