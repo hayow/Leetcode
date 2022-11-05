8081b81ba4b8ed8bf445e7108d6994d8ea0a371a
hayow <Hayow96@gmail.com>
2022-11-05 18:33:03 -0500

    Approach: 1

--- Sliding Window/String/Medium/Leetcode Problem #3.playground/Contents.swift
+++ Sliding Window/String/Medium/Leetcode Problem #3.playground/Contents.swift
@@ -13,3 +13,50 @@
 
 
 
+/*
+
+ Approach: 1
+ Method: Iteration
+ Time complexity: O(m^2 * (n + p)), m == string.count, n == i...j, p == n
+ Space complexity: O(m + n + p), m == _string.count, n == characterSet.count, p == currentSubstring
+ 
+*/
+
+
+
+func approach1(_ string: String) -> Int {
+    
+    func containsDuplicate(_ _string: String) -> Bool {
+        var characterSet: Set<Character> = Set()
+        for character in _string {
+            if characterSet.contains(character) {
+                return true
+            } else {
+                characterSet.insert(character)
+            }
+        }
+        return false
+    }
+    
+    var longestSubstringWithUniqueCharacters: Int = 0
+    
+    for i in 0..<string.count {
+        for j in i..<string.count {
+            var currentSubstring: String = String()
+            for k in i...j {
+                currentSubstring.append(string[string.index(string.startIndex, offsetBy: k)])
+            }
+            
+            if containsDuplicate(currentSubstring) == false {
+                longestSubstringWithUniqueCharacters = max(longestSubstringWithUniqueCharacters, currentSubstring.count)
+            }
+        }
+    }
+    
+    return longestSubstringWithUniqueCharacters
+}
+
+
+
+
+
