dcbfdc836c96a169db60bf28c2170d9334251616
hayow <Hayow96@gmail.com>
2022-11-05 11:12:24 -0500

    Approach: 2

--- Hash Map/Array/Easy/Leetcode Problem #1.playground/Contents.swift
+++ Hash Map/Array/Easy/Leetcode Problem #1.playground/Contents.swift
@@ -39,3 +39,37 @@ func approach1(_ array: [Int], _ target: Int) -> [Int] {
 
 
 
+/*
+
+ Approach: 2
+ Method: Iteration (optimized using 2-pointer)
+ Time complexity: O(nlogn), n == array.count
+ Space complexity: O(m + n), m == array.count, n == m
+ 
+*/
+
+
+
+func approach2(_ array: [Int], _ target: Int) -> [Int] {
+    let sortedArray: [Int] = array.sorted()
+    
+    var leftSidePointer: Int = 0
+    var rightSidePointer: Int = sortedArray.count - 1
+    
+    while leftSidePointer < rightSidePointer {
+        if sortedArray[leftSidePointer] + sortedArray[rightSidePointer] == target {
+            return [leftSidePointer, rightSidePointer]
+        } else if sortedArray[leftSidePointer] + sortedArray[rightSidePointer] < target {
+            leftSidePointer = leftSidePointer + 1
+        } else {
+            rightSidePointer = rightSidePointer - 1
+        }
+    }
+    
+    return []
+}
+
+
+
+
+
