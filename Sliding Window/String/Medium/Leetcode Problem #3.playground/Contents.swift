b4ca07691fbe5f4a47034484b2072d1b0be9bd3c
hayow <Hayow96@gmail.com>
2022-11-05 22:14:48 -0500

    Approach: 2

--- Sliding Window/String/Medium/Leetcode Problem #3.playground/Contents.swift
+++ Sliding Window/String/Medium/Leetcode Problem #3.playground/Contents.swift
@@ -60,3 +60,45 @@ func approach1(_ string: String) -> Int {
 
 
 
+/*
+
+ Approach: 2
+ Method: Iteration (optimized using 2-pointer/sliding window)
+ Time complexity: O(n), n == string.count
+ Space complexity: O(n), n == characterSet.count
+ 
+*/
+
+
+
+func approach2(_ string: String) -> Int {
+    var longestSubstringWithUniqueCharacters: Int = 0
+    var characterSet: Set<Character> = Set()
+    
+    var pointer1: String.Index = string.startIndex
+    var pointer2: String.Index = string.startIndex
+    var i: Int = 0
+    var j: Int = 0
+    
+    while pointer2 != string.endIndex {
+        if characterSet.contains(string[pointer2]) {
+            
+            characterSet.remove(string[pointer1])
+            pointer1 = string.index(after: pointer1)
+            i = i + 1
+        } else {
+            characterSet.insert(string[pointer2])
+            pointer2 = string.index(after: pointer2)
+            j = j + 1
+            longestSubstringWithUniqueCharacters = max(longestSubstringWithUniqueCharacters, j - i)
+        }
+    }
+    
+    
+    return longestSubstringWithUniqueCharacters
+}
+
+
+
+
+
