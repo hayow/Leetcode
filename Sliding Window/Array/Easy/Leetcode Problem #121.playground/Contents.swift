5a899bf7d3a51503a1b6054b6a274bc1529eba17
hayow <Hayow96@gmail.com>
2022-11-07 13:06:23 -0600

    Approach: 2

--- Sliding Window/Array/Easy/Leetcode Problem #121.playground/Contents.swift
+++ Sliding Window/Array/Easy/Leetcode Problem #121.playground/Contents.swift
@@ -18,7 +18,7 @@
  Approach: 1
  Method: Iteration
  Time complexity: O(n*n), n == array.count
- Space complexity: O(2)
+ Space complexity: O(1)
  
 */
 
@@ -32,6 +32,33 @@ func approach1(_ array: [Int]) -> Int {
         for exit in 1 + entry..<array.count {
             maximumProfit = max(maximumProfit, array[exit] - array[entry])
         }
+    }
+    
+    return maximumProfit
+}
+
+
+
+
+
+/*
+
+ Approach: 2
+ Method: Iteration (optimized using 2-pointer)
+ Time complexity: O(n), n == array.count
+ Space complexity: O(1)
+ 
+*/
+
+
+
+func approach2(_ array: [Int]) -> Int {
+    var maximumProfit: Int = 0
+    var entry: Int = 0
+    
+    for exit in 0..<array.count {
+        if array[entry] > array[exit] { entry = exit }
+        maximumProfit = max(maximumProfit, array[exit] - array[entry])
     }
     
     return maximumProfit
