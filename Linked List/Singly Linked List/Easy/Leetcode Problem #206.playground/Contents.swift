6ec711ff769062380cf55b8974b500bcba553ee2
hayow <Hayow96@gmail.com>
2022-11-09 20:19:11 -0600

    Approach: 1

--- Linked List/Singly Linked List/Easy/Leetcode Problem #206.playground/Contents.swift
+++ Linked List/Singly Linked List/Easy/Leetcode Problem #206.playground/Contents.swift
@@ -12,4 +12,43 @@
 
 
 
+// Node class
+class SinglyLinkedListNode {
+    var element: Int
+    var next: SinglyLinkedListNode?
+    
+    init(element: Int, next: SinglyLinkedListNode? = nil) {
+        self.element = element
+        self.next = next
+    }
+}
+
+
+
+
+
+/*
+
+ Approach: 1
+ Method: Recursion
+ Time complexity: O(n), n == length of the linked list
+ Space complexity: O(n), n == length of the linked list
+ 
+*/
+
+
+
+func approach1(_ head: SinglyLinkedListNode?) -> SinglyLinkedListNode? {
+    guard let _: SinglyLinkedListNode = head?.next else { return head }
+    
+    let newHead: SinglyLinkedListNode? = approach1(head?.next)
+    head?.next?.next = head
+    head?.next = nil
+    
+    return newHead
+}
+
+
+
+
 
