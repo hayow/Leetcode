86e1e3bc85ea3f57e044ed17e769b5ad5d8a3ed6
hayow <Hayow96@gmail.com>
2022-11-11 10:01:49 -0600

    Approach: 1

--- Binary Search/Array/Easy/Leetcode Problem #704.playground/Contents.swift
+++ Binary Search/Array/Easy/Leetcode Problem #704.playground/Contents.swift
@@ -13,3 +13,37 @@
 
 
 
+/*
+
+ Approach: 1
+ Method: Iteration
+ Time complexity: O(nlogn), n == array.count
+ Space complexity: O(1)
+ 
+*/
+
+
+
+func approach1(_ array: [Int], _ target: Int) -> Int {
+    var startIndex: Int = 0
+    var endIndex: Int = array.count - 1
+
+
+    while startIndex <= endIndex {
+        let middleIndex: Int = (startIndex + endIndex) / 2
+        if array[middleIndex] == target {
+            return middleIndex
+        } else if array[middleIndex] < target {
+            startIndex = middleIndex + 1
+        } else {
+            endIndex = middleIndex - 1
+        }
+    }
+
+    return -1
+}
+
+
+
+
+
