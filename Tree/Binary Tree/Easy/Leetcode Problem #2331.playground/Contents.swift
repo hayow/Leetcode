994f3757eb2d148d00ed662b4b58fa22d10846be
hayow <Hayow96@gmail.com>
2022-11-17 22:26:57 -0600

    Approach: 1

--- Tree/Binary Tree/Easy/Leetcode Problem #2331.playground/Contents.swift
+++ Tree/Binary Tree/Easy/Leetcode Problem #2331.playground/Contents.swift
@@ -13,3 +13,44 @@
 
 
 
+// Node class
+class TreeNode {
+    var value: Int
+    var leftChild, rightChild: TreeNode?
+    
+    init(_ value: Int, _ leftChild: TreeNode? = nil, _ rightChild: TreeNode? = nil) {
+        self.value = value
+        self.leftChild = leftChild
+        self.rightChild = rightChild
+    }
+}
+
+
+
+
+
+/*
+
+ Approach: 1
+ Method: Recursion
+ Time complexity: O(t), t == size of tree
+ Space complexity: O(t), t == height of tree
+ 
+*/
+
+
+
+func approach1(_ root: TreeNode?) -> Bool {
+    if root?.value == 0 {
+        return false
+    } else if root?.value == 1 {
+        return true
+    } else {
+        return root?.value == 2 ? approach1(root?.leftChild) || approach1(root?.rightChild): approach1(root?.leftChild) && approach1(root?.rightChild)
+    }
+}
+
+
+
+
+
